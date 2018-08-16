d45431658174505a4f63a37d64eef485664c8093
Benjamin Orsini <benjamin.orsini@appaloosa-store.com>
2018-08-17 00:52:48 +0200

    feat: initial version

---
+++ background.js
@@ -0,0 +1,127 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+// A rule
+// host (all, specific)
+// behavior (group, remove dups, trim, pin, archive)
+
+var config = {
+  duplicates: true,
+  group: true,
+  host: true
+}
+
+chrome.runtime.onMessage.addListener(function(message, send, sendResponse) {
+  if(message == 'GET_CONFIG') {
+    sendResponse(config)
+  } else {
+    console.log('new config', message)
+    config = message
+  }
+});
+
+
+chrome.commands.onCommand.addListener(function(command) {
+  if (command == "toggle-pin") {
+    // Get the currently selected tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      // Toggle the pinned status
+      var current = tabs[0]
+      chrome.tabs.update(current.id, {'pinned': !current.pinned});
+    });
+  }
+});
+
+chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+  if(changeInfo.url) {  
+    applyRulesForTab(tab)
+  }
+});
+
+const applyRulesForTab = (tab) => {
+  console.log("apply rules for tab", tab);
+
+  console.log(config)
+
+  groupSameUrlHost(tab)
+    .then(removeDuplicates(tab))
+    .then(trimTabs(tab));
+}
+
+const groupSameUrlHost = (tab) => (
+  new Promise(function(resolve, reject) {
+    if(!config.group) {
+      resolve();
+      return;
+    }
+
+    const url = new URL(tab.url);
+    var hostQuery = url.origin + "/*";
+
+    chrome.tabs.query({url: hostQuery, currentWindow: true}, function(tabs) {
+      const firstIndex = tabs[0].index;
+      tabs.forEach((t, idx) => {
+        console.log("move tab ", idx)
+        chrome.tabs.move(
+          t.id,
+          { index: firstIndex + idx  }
+        );
+      });
+      resolve();
+    });
+  })
+);
+
+const removeDuplicates = (tab) => (
+  new Promise(function(resolve, reject) {
+    if(!config.duplicates) {
+      resolve();
+      return;
+    }
+
+    chrome.tabs.query({url: tab.url, currentWindow: true}, function(tabs) {
+      console.log(tabs.length, "identical tabs")
+      if(tabs.length > 1) {
+        const toRemove = tabs.filter(t => t.id != tab.id);
+        console.log("remove tabs ", toRemove)
+        chrome.tabs.remove(toRemove.map(t => t.id)), function() {
+          resolve();
+        }
+      } else {
+        resolve();
+      }
+    });
+  })
+);
+
+const trimTabs = (tab) => (
+  new Promise(function(resolve, reject) {
+    if(!config.host) {
+      resolve();
+      return;
+    }
+
+    const maxAllowed = 2
+
+    const url = new URL(tab.url);
+    var hostQuery = url.origin + "/*";
+
+    chrome.tabs.query({url: hostQuery, currentWindow: true}, function(tabs) {
+      console.log(tabs.length, "tabs avec le même host")
+      if(tabs.length > maxAllowed) {
+        const toRemove = tabs.filter(t => t.id != tab.id).slice(0, tabs.length - maxAllowed);
+        console.log("trim tab ", toRemove)
+        
+        chrome.tabs.remove(toRemove.map(t => t.id),  function() { 
+          resolve();
+        })
+
+      } else {
+        resolve();
+      }
+    });
+  })
+);
---
+++ popup.js
@@ -0,0 +1,38 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+// A rule
+// host (all, specific)
+// behavior (group, remove dups, trim, pin, archive)
+
+
+
+function $$(id) {
+  return document.querySelector(id);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  var duplicates = $$("input[name=duplicates]")
+  var group = $$("input[name=group]")
+  var host = $$("input[name=host]")
+
+  chrome.runtime.sendMessage(null, 'GET_CONFIG', function(config) {
+    duplicates.checked = config.duplicates
+    group.checked = config.group
+    host.checked = config.host
+  })
+
+  console.log(document)
+
+  document.querySelectorAll('input').forEach(s => s.addEventListener('change', function() {
+      chrome.runtime.sendMessage(null, {
+        duplicates: duplicates.checked,
+        group: group.checked,
+        host: host.checked
+      })
+  }));
+});
